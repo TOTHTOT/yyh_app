58987b78eaa627e3f6296b987954c669279b5ff7
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2024-01-08 19:29:39 +0800

    添加socket代码

---
+++ linux_c/12_socket/socket_tcp_server.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: socket 主机
+ * @Author: TOTHTOT
+ * @Date: 2024-01-08 18:21:50
+ * @LastEditTime: 2024-01-08 18:24:49
+ * @LastEditors: TOTHTOT
+ * @FilePath: \yyh_app\linux_c\12_socket\socket_tcp_server.c
+ */
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
